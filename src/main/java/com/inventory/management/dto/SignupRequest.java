e8f6252c337d7c803ff0c40b79ae3fb6a5077071
Binath Perera <binathpr@gmail.com>
2025-11-03 12:50:07 +0530

    Add cloud mongodb uri. Update software license.

--- src/main/java/com/inventory/management/dto/SignupRequest.java
+++ src/main/java/com/inventory/management/dto/SignupRequest.java
@@ -9,18 +9,18 @@ import java.util.Set;
 
 @Data
 public class SignupRequest {
-    
+
     @NotBlank(message = "Username is required")
     @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
     private String username;
-    
+
     @NotBlank(message = "Email is required")
     @Email(message = "Email must be valid")
     private String email;
-    
+
     @NotBlank(message = "Password is required")
     @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
-    
+
     private Set<String> roles;
 }
