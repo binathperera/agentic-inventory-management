1f6bf21e8fa4ac53d97cc5b0dfe8db7450d5e937
Binath Perera <binathpr@gmail.com>
2025-12-28 19:32:25 +0530

    Update product repository methods

--- src/main/java/com/inventory/management/repository/ProductRepository.java
+++ src/main/java/com/inventory/management/repository/ProductRepository.java
@@ -4,13 +4,7 @@ import com.inventory.management.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface ProductRepository extends MongoRepository<Product, String> {
-    Optional<Product> findBySku(String sku);
-    List<Product> findByCategory(String category);
-    List<Product> findBySupplier(String supplier);
-    Boolean existsBySku(String sku);
+    
 }
